824639e2716335eac7c49c0ac9b6cc1603e6c2c2
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2018-11-27 13:56:50 +0100

    import files projects

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
+package activity;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import ch.heigvd.iict.sym.a3dcompassapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+
+        Button mClickButton1 = findViewById(R.id.NFC);
+        Button mClickButton2 = findViewById(R.id.QRCode);
+        Button mClickButton3 = findViewById(R.id.iBeacon);
+        Button mClickButton4 = findViewById(R.id.capteurs);
+
+
+        mClickButton1.setOnClickListener(onClickListener);
+        mClickButton2.setOnClickListener(onClickListener);
+        mClickButton3.setOnClickListener(onClickListener);
+        mClickButton4.setOnClickListener(onClickListener);
+    }
+
+    // somewhere else in your code
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.NFC: {
+                    break;
+                }
+
+                case R.id.QRCode: {
+                    // do something for button 2 click
+                    break;
+                }
+
+                case R.id.iBeacon: {
+                    // do something for button 1 click
+                    break;
+                }
+
+                case R.id.capteurs: {
+                    // do something for button 2 click
+                    break;
+                }
+            }
+        }
+    };
 }
