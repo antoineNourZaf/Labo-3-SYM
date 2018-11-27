649c4f2f263dd7e62204c71a8f7a42df76458e66
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2018-11-27 14:41:15 +0100

    begins iBeacons activity

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,12 +1,9 @@
 package activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import ch.heigvd.iict.sym.a3dcompassapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
